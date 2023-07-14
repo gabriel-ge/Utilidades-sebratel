50c723651fd4669f3c9531f4221b18859a5629f0
gabriel-ge <gdr.gabrielramos@gmail.com>
2023-07-14 16:12:57 -0300

    Efetuando alterações

--- script.js
+++ script.js
@@ -14,16 +14,23 @@ document.getElementById("copiarBtn").addEventListener("click", function () {
   copiarResultado();
 });
 
+document.getElementById("copiarRelatoBtn").addEventListener("click", function () {
+  copiarRelato();
+});
+
 function exibirDados() {
   var nomeCompleto = document.getElementById("nome").value;
   var sufixoSelecionado = document.getElementById("sufixo").value;
   var resultado = document.getElementById("resultado");
+  var relato = document.getElementById("relato");
+  var programa = document.getElementById("programa").value;
 
   var nomes = nomeCompleto.split(" ");
   var primeiroNome = nomes[0].toLowerCase();
   var ultimoNome = nomes[nomes.length - 1].toLowerCase();
 
   resultado.value = primeiroNome + ultimoNome + sufixoSelecionado;
+  relato.value = "Alterado PPPoE para: " + resultado.value + "Alterado também no: " + programa;
 }
 
 function copiarResultado() {
@@ -31,9 +38,11 @@ function copiarResultado() {
   resultado.select();
   resultado.setSelectionRange(0, resultado.value.length);
   navigator.clipboard.writeText(resultado.value)
-    .then(function () {
-    })
-    .catch(function (error) {
-      console.error("Erro ao copiar o resultado:", error);
-    });
+}
+
+function copiarRelato() {
+  var relato = document.getElementById("relato");
+  relato.select();
+  relato.setSelectionRange(0, relato.value.length);
+  navigator.clipboard.writeText(relato.value)
 }
