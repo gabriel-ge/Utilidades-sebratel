6a591b45d78e77581b246ec5af2922165b1b9d0b
gabriel-ge <gdr.gabrielramos@gmail.com>
2023-07-17 23:59:58 -0300

    Adicionando nova função

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@ document.getElementById("copiarRelatoBtn").addEventListener("click", function ()
   copiarRelato();
 });
 
+document.getElementById("copiarMensagemBtn").addEventListener("click", function () {
+  copiarMensagem();
+});
+
 function exibirDados() {
   var nomeCompleto = document.getElementById("nome").value;
   var sufixoSelecionado = document.getElementById("sufixo").value;
@@ -48,3 +52,22 @@ function copiarRelato() {
 }
 
 let button = document.getElementById("button");
+
+function gerarRelato() {
+  var nomeAtendente = document.getElementById("atendente").value;
+  var regiao = document.getElementById("regiao").value;
+  var prazo = document.getElementById("prazo").value;
+  var quantidadeAfetados = document.getElementById("quantidade").value;
+  var relato = document.getElementById("mensagem");
+
+  relato.value = nomeAtendente + regiao + prazo + quantidadeAfetados;
+
+  relato.value = "Ola, me chamo " + nomeAtendente + "Sou do time do Suporte Técnico da SEBRATEL. Informamos que estamos passando por um evento massivo na região de: " + regiao + ". Nossos técnicos estão trabalhando para finalizar o evento mais rápido possível, visando isso estamos com prazo para as: " + prazo + " horas. Estamos com um total de: " + quantidadeAfetados + " clientes afetados. A SEBRATEL agradece a compreensão!"
+}
+
+function copiarResultado() {
+  var mensagem = document.getElementById("mensagem");
+  mensagem.select();
+  mensagem.setSelectionRange(0, mensagem.value.length);
+  navigator.clipboard.writeText(mensagem.value)
+}
